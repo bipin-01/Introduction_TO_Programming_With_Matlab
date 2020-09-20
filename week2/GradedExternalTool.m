8331bda5e28d385c1ffb28fb1a00d76578b26d20
bipin-01 <50213989+bipin-01@users.noreply.github.com>
2020-09-20 22:42:55 +0530

    Create GradedExternalTool.m

---
+++ week2/GradedExternalTool.m
@@ -0,0 +1,14 @@
+%{ 
+    @uthor:Bipin Poudel
+%}
+
+%solution to question no.1
+distance = 100/1000;                             % 100 m changed to kilometer/km
+time = 9.58 /3600;                               % changing 9.58 sec to hour
+hundred = distance / time
+
+%solution to question number two
+time_hr = (2*3600+1*60+39)/3600;
+distance = 42.195;
+marathon = distance/time_hr
+
