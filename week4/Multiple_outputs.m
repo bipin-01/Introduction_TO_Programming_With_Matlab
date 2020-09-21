b7652ac98040131c9041ab33d7fbbbdd239eef7a
bipin-01 <50213989+bipin-01@users.noreply.github.com>
2020-09-21 14:59:48 +0530

    assesment

---
+++ week4/Multiple_outputs.m
@@ -0,0 +1,9 @@
+%{ 
+    @uthor:Bipin Poudel
+%}
+function [top_left, top_right, bottom_left, bottom_right]  = corners(A)
+    top_left = A(1,1);
+    top_right = A(1, end);
+    bottom_left = A(end, 1);
+    bottom_right = A(end, end);
+end
